c1c77cdcdacdb9732531f903bd9184c0b2773faa
Thomas McManus <tmcmanus@blueprint-ade.ca>
2022-08-29 15:51:08 -0400

    doc + build

--- assignment-rounds/round-02.R
+++ assignment-rounds/round-02.R
@@ -3,6 +3,9 @@ library(readxl)
 library(npowerassignment)
 
 # Examine raw data to check for required changes
+# Changes to raw data:
+# - Created column to track previous randomization
+# - 
 data_dir <- "Z:/FSC - NPower/data-2022/transfer/Cohort Applicants August 2022"
 
 data_path <- fs::dir_ls(data_dir)[1]
@@ -13,8 +16,12 @@ offer_nums <- read_excel(data_path, sheet = "Number to be randomized in")
 
 locations <- unique(dat_raw$`Program Offered in:`)
 
+locations
+
 update_stratification_parameters(list(eligible_locations = "Greater Toronto Area - GTA, ON;Calgary, AB"))
 
 params <- get_latest_stratification_parameters()
 
 params
+
+
